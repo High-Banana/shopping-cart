5051b56328c527b9879e2377c2806b20bfa0a6b5
Potassium <brainiacgamer444@gmail.com>
2024-01-13 02:39:14 +0545

    Style navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="flex gap-[20px]">
-      <Link to="/">Home</Link>
-      <Link to="about">About</Link>
-      <Link to="products">Products</Link>
-      <Link to="cart">Cart</Link>
-      <Link to="login">Login</Link>
+    <div className="flex justify-between bg-black/75 text-white py-[20px] px-[100px] backdrop-blur-lg">
+      <div className="flex gap-[30px] text-[20px] font-[500]">
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="products">Products</Link>
+      </div>
+      <div className="flex gap-[30px] text-[20px] font-[500]">
+        <Link to="cart">Cart</Link>
+        <Link to="login">Login</Link>
+      </div>
     </div>
   );
 }
