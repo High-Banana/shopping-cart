402e0d919801412385eff9100f97e2048b75052f
Potassium <brainiacgamer444@gmail.com>
2024-01-14 20:43:06 +0545

    Create basic layout for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <div className="bg-[#181617] text-white flex justify-around">
+      <div className="py-[50px] w-[300px]">
+        <img src="public/shopping-cart-icon.jpg" className="w-[110px] h-[100px]" />
+      </div>
+      <div className="py-[50px]">
+        <h1 className="text-3xl font-semibold">USEFUL LINKS</h1>
+        <ul className="text-[#c1c1c1] flex flex-col gap-[10px] py-[10px]">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+          <li>
+            <Link to="products">Products</Link>
+          </li>
+          <li>
+            <Link to="https://github.com/High-Banana/shopping-cart" target="_blank">
+              Repository
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
--- src/pages/Product/ProductPage.jsx
+++ src/pages/Product/ProductPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import Card from "../../components/Card/Card";
 import { fetchApi } from "../../services/api/Fetch";
