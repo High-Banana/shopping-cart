3319a01896bb4982305ca8e54fc8a91e75408044
Potassium <brainiacgamer444@gmail.com>
2024-02-05 15:46:46 +0545

    Create functions to get products from database and add products to database

--- server/database.js
+++ server/database.js
@@ -1,11 +1,40 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
+const database = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+async function getAllProducts() {
+  try {
+    const [products] = database.query("select * from products");
+    return products;
+  } catch (error) {
+    return `An error occured: ${error}`;
+  }
+}
+
+async function getProduct(productName) {
+  try {
+    const [product] = await database.query("select * from products where product_name = ?", [productName]);
+    return product;
+  } catch (error) {
+    return `An error occured: ${error}`;
+  }
+}
+
+async function addProduct(name, price, description) {
+  const id = uuidv4();
+  await database.query("insert into products values(?, ?, ?, ?)", [id, name, price, description]);
+}
+
+// console.log(await getProduct("L"));
+// addProduct("I phone 9", 800, "Mobile phone for future");
