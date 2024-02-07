28e471dbec3df989b4b3d43c55642982bc1f41ac
Potassium <brainiacgamer444@gmail.com>
2024-02-07 09:55:08 +0545

    Create api to fetch image from database and convert the image into src string
    The reason to convert the image into src string is so that the image can displayed as url in the webpage

--- server/database.js
+++ server/database.js
@@ -2,8 +2,17 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 import { v4 as uuidv4 } from "uuid";
 import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+
+app.use("/db_images", express.static("./db_images"));
 
 dotenv.config();
 
@@ -34,16 +43,32 @@ async function getProduct(productName) {
   }
 }
 
-async function addProduct(name, price, description) {
-  const id = uuidv4();
-  await database.query("insert into products values(?, ?, ?, ?)", [id, name, price, description]);
+async function addProduct(name, description, price, image) {
+  await database.query("insert into products values(?, ?, ?, ?, ?)", [uuidv4(), name, description, price, image]);
 }
 
-// console.log(await getProduct("L"));
-// addProduct("I phone 9", 800, "Mobile phone for future");
+// addProduct(
+//   "ASUS ZenBook 14",
+//   "14'' Display in 13'' Chassis. 92% Screen-to-Boddy Ratio. 3D IR Camera. Military-grade quality",
+//   1000,
+//   "./db-images/asus_zenbook_14.png"
+// );
 
 app.get("/api/products", async (req, res) => {
   res.send(await getAllProducts());
+});
+
+app.get("/api/db_images/:image", (req, res) => {
+  const imageName = req.params.image;
+  const imagePath = path.join(__dirname, "db_images", imageName);
+  fs.readFile(imagePath, (err, data) => {
+    if (err) return console.log(err);
+    else {
+      const base64Image = Buffer.from(data).toString("base64");
+      const imgSrcString = "data:image/jpeg;base64," + base64Image;
+      res.send(imgSrcString);
+    }
+  });
 });
 
 app.listen(process.env.PORT, () => console.log("listening"));
