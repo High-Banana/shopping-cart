c0559987b1337ecfda9eae21ce97f2ed4fdae04d
Potassium <brainiacgamer444@gmail.com>
2024-02-07 09:56:50 +0545

    Put correct url in image src
    The image fetched in products page will send image string src which is then displayed from card components

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 export default function Card({ products }) {
+  const BASE_URL = "http://localhost:5000";
   return (
     <div className="grid grid-cols-2">
       {products.map((product) => {
         return (
-          <div key={product.product_id}>
+          <div key={product.id}>
             <h1>{product.product_name}</h1>
-            <h1>{product.product_price}</h1>
+            <h1>${product.product_price}</h1>
             <h1>{product.product_description}</h1>
+            <img src={`${BASE_URL}/db_images/${product.image}`} alt={product.product_name} />
           </div>
         );
       })}
