6213f184d0efcfcb85ffe6cf334c619f613ea951
Potassium <brainiacgamer444@gmail.com>
2024-02-15 21:55:05 +0545

    Setup router for single product to get displayed

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
 export default function Card({ products }) {
   const BASE_URL = "http://localhost:5000";
   return (
@@ -6,17 +8,19 @@ export default function Card({ products }) {
       {products.map((product) => {
         return (
           <div key={product.id} className="flex flex-col h-full transition duration-[0.3s] hover:translate-y-[-10px]">
-            <img
-              src={`${BASE_URL}/db_images/${product.image}`}
-              alt={product.product_name}
-              className="h-[300px] w-full object-contain"
-            />
-            <div className="bg-[#dcdcdc] rounded-[8px] p-[10px] flex flex-col gap-[15px]">
-              <h1 className="text-[20px] font-[700]">{product.product_name}</h1>
-              <span className="text-[18px] font-[600]">NPR {parseFloat(product.product_price).toLocaleString("en-US")}</span>
-              <span className="font-[500] text-[#5f5f5f]">{product.product_type}</span>
-              {/* <h1>{product.product_description}</h1> */}
-            </div>
+            <Link to={`product/${product.id}`}>
+              <img
+                src={`${BASE_URL}/db_images/${product.image}`}
+                alt={product.product_name}
+                className="h-[300px] w-full object-contain"
+              />
+              <div className="bg-[#dcdcdc] rounded-[8px] p-[10px] flex flex-col gap-[15px]">
+                <h1 className="text-[20px] font-[700]">{product.product_name}</h1>
+                <span className="text-[18px] font-[600]">NPR {parseFloat(product.product_price).toLocaleString("en-US")}</span>
+                <span className="font-[500] text-[#5f5f5f]">{product.product_type}</span>
+                {/* <h1>{product.product_description}</h1> */}
+              </div>
+            </Link>
           </div>
         );
       })}
--- client/src/pages/Product/ProductPage.jsx
+++ client/src/pages/Product/ProductPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Card from "../../components/Card/Card";
 import { fetchApi } from "../../services/api/Fetch";
 import Loading from "../../components/Loading/Loading";
+import { Outlet } from "react-router-dom";
 
 export default function ProductPage() {
   const [products, setProducts] = useState();
@@ -25,6 +26,7 @@ export default function ProductPage() {
         <>
           <h1 className="font-[700] text-[20px]">{products.length} items</h1>
           <Card products={products} />
+          <Outlet />
         </>
       )}
     </div>
