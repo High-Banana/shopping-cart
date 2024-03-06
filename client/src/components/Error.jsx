7215fce5ebfd888cff3867bbad314337f9e1b8f3
Potassium <brainiacgamer444@gmail.com>
2024-03-06 23:25:22 +0545

    Change UI of error component

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -2,11 +2,12 @@
 import Button from "./ui/Button";
 
 export default function Error({ onClickFunction, errorDetail }) {
-  console.log(errorDetail.response.status);
   return (
     <div className="flex justify-center items-center min-h-screen flex-col gap-[50px]">
-      <h1 className="text-4xl font-semibold tracking-wider">An error occured while loading the data!</h1>
-      <span className="font-semibold text-2xl">Status Code: {errorDetail.response.status}</span>
+      <div className="flex text-7xl gap-[20px] items-center">
+        <span className="text-6xl">{errorDetail.response.data}</span> |
+        <span className="text-6xl">{errorDetail.response.statusText}</span>
+      </div>
       <Button title="Try Again" className="bg-[#d72c2c]" onClick={onClickFunction} />
     </div>
   );
