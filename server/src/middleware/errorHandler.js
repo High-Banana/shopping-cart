b8e3fe093d75c47a50f70c7936ba6841ac574833
Potassium <brainiacgamer444@gmail.com>
2024-03-06 23:40:37 +0545

    Add default value for status code

--- server/src/middleware/errorHandler.js
+++ server/src/middleware/errorHandler.js
@@ -1,5 +1,5 @@
 export default function errorHandler(error, request, response, next) {
-  const statusCode = parseInt(error.message.slice(0, 4));
+  const statusCode = parseInt(error.message.slice(0, 4)) || 500;
   if (!isNaN(statusCode)) response.status(statusCode).send(error.message);
   else response.status(503).send(error.message);
 }
