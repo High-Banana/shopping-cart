cfb0f97de41878b02cbd4e7d6ada87b4688fa530
Potassium <brainiacgamer444@gmail.com>
2024-03-14 14:26:53 +0545

    Display response status instead of response data
    The reason for this change was response.data would display nothing when backend is not running because there is no data to display.

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -5,7 +5,7 @@ export default function Error({ onClickFunction, errorDetail }) {
   return (
     <div className="flex justify-center items-center min-h-screen flex-col gap-[50px]">
       <div className="flex text-7xl gap-[20px] items-center">
-        <span className="text-6xl">{errorDetail.response.data}</span> |
+        <span className="text-6xl">{errorDetail.response.status} Error</span> |
         <span className="text-6xl">{errorDetail.response.statusText}</span>
       </div>
       <Button title="Try Again" className="bg-[#d72c2c]" onClick={onClickFunction} />
