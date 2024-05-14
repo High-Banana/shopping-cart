d566d25e118e022d5bda5da913ef024c0a856ae1
Potassium <brainiacgamer444@gmail.com>
2024-05-14 20:43:36 +0545

    Create search bar and style it

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,20 +4,33 @@ import { Link } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
 import { CiShoppingCart } from "react-icons/ci";
 import { useUserContext } from "../../context/UserContext";
+import { IoSearchOutline } from "react-icons/io5";
 
 export default function Navbar() {
   const { getTotalItems, toggleOpenCart } = useCart();
   const { user } = useUserContext();
   const [isVisible, setIsVisible] = React.useState(true);
   const [lastScrollPosition, setLastScrollPosition] = React.useState(0);
+  const [isSearchActive, setIsSearchActive] = React.useState(false);
+  const searchBarRef = React.useRef(null);
 
   function handleNavbarScroll() {
     const currentScrollPosition = window.scrollY;
     if (currentScrollPosition > lastScrollPosition) setIsVisible(false);
     else if (lastScrollPosition > currentScrollPosition) setIsVisible(true);
-
     setLastScrollPosition(currentScrollPosition);
   }
+
+  function handleSearchBar(event) {
+    if (searchBarRef.current && searchBarRef.current.contains(event.target))
+      // searchBarRef.current.style = "width:300px;box-shadow: 0 0 15px 4px black";
+      setIsSearchActive(true);
+    else setIsSearchActive(false);
+  }
+
+  React.useEffect(() => {
+    document.addEventListener("click", handleSearchBar);
+  }, []);
 
   React.useEffect(() => {
     window.addEventListener("scroll", handleNavbarScroll);
@@ -36,6 +49,16 @@ export default function Navbar() {
         <Link to="products" className="navbar-link-hover">
           Products
         </Link>
+      </div>
+      <div
+        ref={searchBarRef}
+        className={`flex bg-white px-2 rounded-lg justify-between gap-[5px] duration-300 transition-all ${
+          isSearchActive ? "w-[350px] shadow-black shadow-[0_0_15px_4px]" : "w-[200px]"
+        }`}>
+        <input placeholder="Search products" className="w-full text-black font-semibold outline-none"></input>
+        <button className="text-black text-2xl">
+          <IoSearchOutline />
+        </button>
       </div>
       <div className="flex gap-[30px] text-[20px] font-[500]">
         <button className="flex items-start navbar-link-hover" onClick={() => toggleOpenCart()}>
