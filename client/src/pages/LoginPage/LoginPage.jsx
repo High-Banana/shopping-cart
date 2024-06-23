7ea61695902b3798f569e0d50d15317d14da4a32
Potassium <brainiacgamer444@gmail.com>
2024-06-23 16:12:17 +0545

    Navigate back to previous page after log in
    Brought this feature back for a while. Can be changed in future to navigate to profile page instead

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
   const [isLoginForm, setIsLoginForm] = React.useState(true);
 
   React.useEffect(() => {
-    if (userDetails.username !== null && userDetails && !isLoading) navigate("/profile");
+    if (userDetails.username !== null && userDetails && !isLoading) navigate(-1);
     console.log(userDetails);
   }, [userDetails, isLoading]);
 
